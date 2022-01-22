55ef502a43e06dc8fe08ad7a5e243dce9b54d580
saleh <saleh_jamali@ymail.com>
2022-01-22 15:56:18 -0500

    cpp submodules

---
+++ cpp/inc/GlobalHelpers.h
@@ -0,0 +1,35 @@
+//
+// Created by saleh on 1/22/22.
+//
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <execinfo.h>
+#include <unistd.h>
+#include <csignal>
+#include "spdlog/spdlog.h"
+
+extern spdlog::logger *logger;
+extern string globalArgDataPath;
+extern unsigned globalBatchsize;
+extern bool globalDumpTensors;
+
+extern void SetupModules(int argc, const char* argv[]);
+
+#define ThrowException(arg) \
+    {\
+    std::string _msg = CStringFormatter()<<__FILE__<<":"<<__LINE__<<": "<<arg;\
+    throw std::runtime_error(_msg); \
+    SPDLOG_LOGGER_ERROR(logger,_msg);\
+    exit(EXIT_FAILURE);\
+    }
+
+#define ConditionCheck(condition,msgIfFalse) \
+    if(!(condition)){\
+    std::string _msg = CStringFormatter()<<__FILE__<<":"<<__LINE__<<": Failed "<< #condition <<": "<<msgIfFalse;\
+    throw std::runtime_error(_msg); \
+    SPDLOG_LOGGER_ERROR(logger,_msg);\
+    exit(EXIT_FAILURE);\
+    }
---
+++ cpp/src/GlobalHelpers.cpp
@@ -0,0 +1,124 @@
+//
+// Created by saleh on 1/22/22.
+//
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include "argparse.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
+#include "GlobalHelpers.h"
+
+using namespace std;
+using namespace argparse;
+
+spdlog::logger *logger;
+string globalArgDataPath;
+unsigned globalBatchsize;
+bool globalDumpTensors=false;
+
+void Handler(int sig) {
+  void *array[40];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 40);
+
+  // print out all the frames to stderr
+  cerr<<"The host program has crashed, printing call stack:\n";
+  cerr<<"Error: signal "<< sig<<"\n";
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+
+  SPDLOG_LOGGER_CRITICAL(logger,"The host program has crashed.");
+  spdlog::shutdown();
+
+  exit(SIGSEGV);
+}
+
+void HandlerInt(int sig_no)
+{
+  SPDLOG_LOGGER_CRITICAL(logger,"CTRL+C pressed, terminating...");
+  spdlog::shutdown();
+  exit(SIGINT);
+}
+
+void SetupModules(int argc, const char* argv[]){
+  signal(SIGSEGV, Handler);
+  signal(SIGABRT, Handler);
+  signal(SIGINT, HandlerInt);
+
+  ArgumentParser parser(argv[0], "Inference");
+
+  parser.add_argument()
+      .names({"-d", "--data"})
+      .description("Dump directory")
+      .required(true);
+
+  parser.add_argument()
+      .names({"-b", "--batchsize"})
+      .description("Batch-size")
+      .required(false);
+
+  parser.add_argument()
+      .names({"-k", "--dumptensors"})
+      .description("Dump tensors into *.npy files in the dump directory (no value is needed for this argument)")
+      .required(false);
+
+  parser.add_argument()
+      .names({"-n", "--nolog"})
+      .description("Disable logging (no value is needed for this argument)")
+      .required(false);
+
+  parser.enable_help();
+  auto err = parser.parse(argc, argv);
+  if(err){
+    std::cerr << err << std::endl;
+    parser.print_help();
+    exit(EXIT_FAILURE);
+  }
+
+  if(parser.exists("help")){
+    parser.print_help();
+    exit(EXIT_SUCCESS);
+  }
+
+  {
+    // HOST LOGGER
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::trace);
+    console_sink->set_pattern("[%H:%M:%S.%e][%^%l%$] %v");
+
+    auto file_sink1 = std::make_shared<spdlog::sinks::basic_file_sink_mt>("hostlog_0trace.log", true);
+    file_sink1->set_level(spdlog::level::trace);
+    file_sink1->set_pattern("[%H:%M:%S.%f][%^%l%$][source %s][function %!][line %#] %v");
+
+
+    auto file_sink3 = std::make_shared<spdlog::sinks::basic_file_sink_mt>("hostlog_3wraning.log", true);
+    file_sink3->set_level(spdlog::level::warn);
+    file_sink3->set_pattern("[%H:%M:%S.%f][%^%l%$][source %s][function %!][line %#] %v");
+
+    logger = new spdlog::logger("Inference", {console_sink, file_sink1, file_sink3});
+    logger->set_level(spdlog::level::trace);
+
+    if(parser.exists("n")) {
+      logger->set_level(spdlog::level::off);
+    }
+  }
+
+  if(parser.exists("b")) {
+    globalBatchsize = parser.get<unsigned>("b");
+  }else{
+    globalBatchsize = 100;
+  }
+  SPDLOG_LOGGER_INFO(logger,"Batch-size: {}", globalBatchsize);
+
+  if(parser.exists("d")) {
+    globalArgDataPath = parser.get<string>("d");
+    SPDLOG_LOGGER_INFO(logger,"Dump Directory: {}", globalArgDataPath);
+  }
+
+  if(parser.exists("dumptensors")) {
+    globalDumpTensors = true;
+    SPDLOG_LOGGER_INFO(logger,"Tensors will be dumped into separate numpy files in the data directory.");
+  }
+}
+
